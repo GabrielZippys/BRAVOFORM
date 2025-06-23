af1b4c123f660659506bf1ad2657ea2042a8ea2e
Gabriel Henrique Lopes de Oliveira <mataldercraft56@gmail.com>
2025-06-23 15:28:17 -0300

    Integrations 10

--- firebase/config.js
+++ firebase/config.js
@@ -1,10 +1,8 @@
-// Importe as funções que você precisa dos SDKs que você precisa
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// A configuração do seu app Firebase da web
-// CORREÇÃO: Removidos possíveis caracteres invisíveis e garantida a formatação correta.
+// O código agora lê as chaves de variáveis de ambiente seguras
 const firebaseConfig = {
   apiKey: "AIzaSyD5QTf_loBVvL56v_I5LN-pXNCQXjVfvy4",
   authDomain: "formbravo-8854e.firebaseapp.com",
@@ -14,10 +12,7 @@ const firebaseConfig = {
   appId: "1:1047816908015:web:0b812eeee23eca21b6ff1a"
 };
 
-// Inicializa o Firebase
 let app;
-
-// Evita a reinicialização do app no Next.js em ambiente de desenvolvimento
 if (!getApps().length) {
   app = initializeApp(firebaseConfig);
 } else {
