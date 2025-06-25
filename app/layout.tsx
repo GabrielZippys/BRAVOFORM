372b7404daa411c3b18d0d26f7c144069b207e37
Gabriel Henrique Lopes de Oliveira <gabriel.henrique@bravo-ti.com>
2025-06-25 18:29:32 -0300

    Teste 7

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,14 @@
-
 import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "FORMBRAVO",
+  title: "Cependant Formulários",
   description: "Plataforma Inteligente de Formulários",
 };
 
-import { ReactNode } from "react";
-
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <head>
-        <meta charSet="UTF-8" />
-        <title>Cependant Formulários</title>
-      </head>
       <body>{children}</body>
     </html>
   );
