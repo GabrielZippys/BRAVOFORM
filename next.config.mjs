e799e9f17173203b9454d0a31aeb3e810669e673
Gabriel Henrique Lopes de Oliveira <gabriel.henrique@bravo-ti.com>
2025-06-25 22:15:26 -0300

    Teste 13

--- next.config.mjs
+++ next.config.mjs
@@ -1,2 +1,23 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Adicione esta linha
+  },
+};
 
+export default nextConfig;
