eafcff95dcb4ee8bc09500da38491ccb47933949
Gabriel Henrique Lopes de Oliveira <mataldercraft56@gmail.com>
2025-07-01 07:49:07 -0300

    Nome barra

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,27 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
+// O AuthProvider foi removido, pois não é utilizado no seu projeto.
+
+const inter = Inter({ subsets: ["latin"] });
+
 export const metadata: Metadata = {
-  title: "Cependant Formulários",
-  description: "Plataforma Inteligente de Formulários",
+  title: "BRAVOFORM",
+  description: "Plataforma de formulários Bravo.",
 };
 
-import { ReactNode } from "react";
-
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body className={inter.className}>
+        {/* Renderiza diretamente os componentes filhos sem o AuthProvider */}
+        {children}
+      </body>
     </html>
   );
 }
